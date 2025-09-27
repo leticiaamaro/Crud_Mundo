19b756d99a01c28ca40761089a2d33d8a8999e90
Letícia Amaro <101998517+leticiaamaro@users.noreply.github.com>
2025-09-27 13:14:59 -0300

    Add pastas
    Adição das pastas:
    - database (banco de dados)
    - web (aplicações web)

---
+++ database/bd_mundo.sql
@@ -0,0 +1,20 @@
+-- Criação do banco de dados
+Create database  bd_mundo;
+use bd_mundo;
+
+-- Criação de tabelas
+create table paises (
+    id_pais int primary key auto_increment,
+    nome varchar(100) not null,
+    continente varchar(50) not null,
+    populacao int not null,
+    idioma varchar(50) not null
+);
+
+create table cidades (
+    id_cidade int primary key auto_increment,
+    nome varchar(100) not null,
+    populacao int not null,
+    id_pais int not null,
+    foreign key (id_pais) references paises(id_pais)
+);

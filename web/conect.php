4f33e4769c23de580168318ac2ff9c1919a7a138
Letícia Amaro <101998517+leticiaamaro@users.noreply.github.com>
2025-11-08 16:13:21 -0300

    Update conect.php

--- web/conect.php
+++ web/conect.php
@@ -1,1 +1,12 @@
+<?php  
+	$host = '127.0.0.1'; //:3307
+	$usuario_db = "root"; 
+	$senha_db = "";       
+	$banco = "bd_mundo";
+	$conn = new mysqli($host, $usuario_db, $senha_db, $banco); //criação conexão
 
+	// verificaçõa conexão
+	if ($conn->connect_error) {
+		die("Erro de conexão: " . $conn->connect_error);
+	}
+?>
